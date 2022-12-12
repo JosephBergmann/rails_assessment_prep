5d4bba25126c78c462bddb0b507ab1a57468f28c
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-12 17:42:23 -0500

    finished another practice assessment

--- rails1p-starter_2/associations/app/models/comment.rb
+++ rails1p-starter_2/associations/app/models/comment.rb
@@ -11,4 +11,29 @@
 #  updated_at        :datetime         not null
 #
 class Comment < ApplicationRecord
+    belongs_to :commenter,
+        primary_key: :id,
+        foreign_key: :commenter_id,
+        class_name: :User
+
+    belongs_to :video,
+        primary_key: :id,
+        foreign_key: :video_id,
+        class_name: :Video
+
+    belongs_to :parent_comment,
+        primary_key: :id,
+        foreign_key: :parent_comment_id,
+        class_name: :Comment,
+        optional: true
+        
+    has_many :replies,
+        primary_key: :id,
+        foreign_key: :parent_comment_id,
+        class_name: :Comment,
+        dependent: :destroy
+    
+    has_one :parent_commenter,
+        through: :parent_comment,
+        source: :commenter
 end
--- rails1p-starter_2/associations/app/models/video.rb
+++ rails1p-starter_2/associations/app/models/video.rb
@@ -10,4 +10,32 @@
 #  updated_at  :datetime         not null
 #
 class Video < ApplicationRecord
+    belongs_to :uploader,
+        primary_key: :id,
+        foreign_key: :uploader_id,
+        class_name: :User
+
+    has_many :comments,
+        primary_key: :id,
+        foreign_key: :video_id,
+        class_name: :Comment,
+        dependent: :destroy
+    
+    has_many :likes,
+        primary_key: :id,
+        foreign_key: :video_id,
+        class_name: :Like,
+        dependent: :destroy
+    
+    has_many :likers,
+        through: :likes,
+        source: :liker
+
+    has_many :commenters,
+        through: :comments,
+        source: :commenter
+    
+    has_many :likers_comments,
+        through: :likers,
+        source: :comments
 end
